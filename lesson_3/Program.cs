c65676ccece68dfd415ed3f371274d2478a22731
diana-yu <bambi.diyu@gmail.com>
2022-11-01 19:41:45 +0200

    add lesson 3 practice

---
+++ lesson_3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    int a;
+    int b;
+    //stringname = Сума;
+
+    Console.WriteLine("Введи число 1");
+    string input1 = Console.ReadLine();
+    Console.WriteLine("Введи число 2");
+    string input2 = Console.ReadLine();
+    a = int.Parse(input1);
+    b = int.Parse(input2);
+    Console.WriteLine("Сума " + (a + b));
+    Console.WriteLine(a - b);
+    Console.WriteLine(a * b);
+    Console.WriteLine((double) a / b);
