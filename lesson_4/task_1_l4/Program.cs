5c1c1c49e0b55a150153bcf220a3d95829ad1900
diana-yu <bambi.diyu@gmail.com>
2022-11-03 19:35:13 +0200

    lesson 4

---
+++ lesson_4/task_1_l4/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введи значення!");
+        string word = Console.ReadLine();
+
+        // if (word == "яблуко")
+        // {
+        //     Console.WriteLine("apple");
+        // }
+        // else if (word == "привт")
+        // {
+        //     Console.WriteLine("hi");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Не знаю");    
+        // }
+
+        switch (word)
+            {
+                case "яблуко":
+                    Console.WriteLine("apple");
+                    break;
+                case "небо":
+                    Console.WriteLine("sky");
+                    break;
+                default:
+                    Console.WriteLine("Не знаю!");
+                    break;
+            }
+    }
+}
