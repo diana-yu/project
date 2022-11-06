acdbc43ab669a632efd38c39731114c0308d93b6
diana-yu <bambi.diyu@gmail.com>
2022-11-06 15:50:36 +0200

    add homework 4

---
+++ homework_4/task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введи число від 0 до 100!");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number <= 14)
+        {
+            Console.WriteLine("[0 - 14]");
+        }
+        else if (number > 14 & number <= 35)
+        {
+            Console.WriteLine("[15 - 35]");
+        }
+        else if (number > 35 & number <= 50)
+        {
+            Console.WriteLine("[36 - 50]");
+        }
+        else if (number >= 50 & number <= 100)
+        {
+            Console.WriteLine("[51 - 100]");
+        }
+        else
+        {
+            Console.WriteLine("Не входить в 0-100");    
+        }
+    }
+}
