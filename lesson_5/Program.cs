062d13eade0238e8086bfdb6781fdaf125317554
diana-yu <bambi.diyu@gmail.com>
2022-11-08 19:36:28 +0200

    lesson 5

---
+++ lesson_5/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random r = new Random();
+
+        int height = r.Next(6,33);
+        int width = r.Next(1,9);
+
+        for (int rowCounter = 0; rowCounter < height; rowCounter++)
+        {
+            for (int columnCounter = 0; columnCounter < width; columnCounter++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
