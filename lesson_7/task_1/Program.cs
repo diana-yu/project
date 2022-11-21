dd90a412985ec6dbb55a5410021c7382736b1063
diana-yu <bambi.diyu@gmail.com>
2022-11-21 21:31:12 +0200

    lesson7

---
+++ lesson_7/task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = {10,19,8,7,-6,5,4,578,2,1,1000000,7,456,567789543};
+        int max = int.MinValue;
+        
+        for (int i = array.Length - 1; i >= 0 ; i--)
+        {
+           if(array[i] > max)
+           {
+                max = array [i];
+           }           
+        }
+
+        Console.WriteLine(max);
+    }
+}
