37f492ec2ea11acf172239d7931a76340dc3e0fa
diana-yu <bambi.diyu@gmail.com>
2022-11-22 23:29:32 +0200

    homework6

---
+++ homework_6/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static double Convert(double sum, double rate)
+        {
+            return sum * rate;
+        }
+        
+        static void Main()
+        {
+            double sum;
+            double rate;
+            
+            Console.WriteLine("Enter the amount");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the currency conversion rate");
+            string input2 = Console.ReadLine();
+
+            sum = double.Parse(input1);
+            rate = double.Parse(input2);
+
+            double convertResult = Convert(sum, rate);
+
+            Console.WriteLine($"Convertation rate {convertResult}");
+
+            Console.ReadKey();
+        }
+    }
+}
