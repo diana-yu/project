6006e9a902fe05f593e99ef0bdb386ec11c67614
diana-yu <bambi.diyu@gmail.com>
2022-11-22 23:29:03 +0200

    homework6

---
+++ homework_6/task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static string Compare(int number)
+
+       
+        {
+            string positiveNumber = number > 0 ? "positive":"negative or zero";
+
+            string divisibleNumber = number %  2 == 0 ? "divisible by 2 without remainder" : "not divisible by 2 without remainder";  
+                                    
+            return ($"The number is {positiveNumber} and {divisibleNumber}");
+        }
+
+        static void Main()
+        {
+            int number;
+
+            Console.WriteLine("Enter number");
+            string input1 = Console.ReadLine();
+
+            number = int.Parse(input1);
+
+            string result = Compare(number);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
